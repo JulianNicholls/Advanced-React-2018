5dbacf265eb94b67eea69ce26236da7a28b77aab
Julian Nicholls <juliannicholls29@gmail.com>
2018-07-15 22:29:32 +0100

    Initial Tests

---
+++ testing/src/components/__tests__/App.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import App from '../App';
+import CommentBox from '../CommentBox';
+import CommentList from '../CommentList';
+
+describe('App', () => {
+  it('contains a CommentBox component', () => {
+    const wrapped = shallow(<App />);
+
+    // Check that the component is there
+    expect(wrapped.find(CommentBox).length).toEqual(1);
+  });
+
+  it('contains a CommentList component', () => {
+    const wrapped = shallow(<App />);
+
+    // Check that the component is there
+    expect(wrapped.find(CommentList).length).toEqual(1);
+  });
+});
