2893d45d84980ba1cd745fdfb2da08126de06325
Julian Nicholls <juliannicholls29@gmail.com>
2018-07-22 10:38:20 +0100

    Wire up auth reducer

--- auth/client/src/index.js
+++ auth/client/src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import App from 'components/App';
 import Welcome from 'components/Welcome';
 import Signup from 'components/auth/Signup';
+import reducers from 'reducers';
+
+const store = createStore(reducers, {});
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App>
-      <Route path="/" exact component={Welcome} />
-      <Route path="/signup" component={Signup} />
-    </App>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App>
+        <Route path="/" exact component={Welcome} />
+        <Route path="/signup" component={Signup} />
+      </App>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
---
+++ auth/client/src/reducers/auth.js
@@ -0,0 +1,11 @@
+const INITIAL_STATE = {
+  authenticated: '',
+  errorMessage: ''
+};
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
