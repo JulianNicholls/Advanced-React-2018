2379baa94e4edddc04b5ac9393282709d56786cf
Julian Nicholls <juliannicholls29@gmail.com>
2018-07-23 16:14:09 +0100

    Install and use cors module

--- auth/server/index.js
+++ auth/server/index.js
@@ -5,6 +5,7 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const router = require('./router');
 
@@ -14,12 +15,14 @@ mongoose.connect(
   'mongodb://127.0.0.1:27017/auth',
   { useNewUrlParser: true }
 );
+
 // App setup
 
 const app = express();
 
 app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*' }));
+app.use(cors({ origin: 'http://localhost:3000' }));
 
 router(app);
 
