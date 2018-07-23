609e84bd25ef0efe80eba72d3edde196b62efe87
Julian Nicholls <juliannicholls29@gmail.com>
2018-07-23 12:50:32 +0100

    Add reducer and use redux-form for signup.

--- auth/client/src/components/auth/Signup.js
+++ auth/client/src/components/auth/Signup.js
@@ -1,18 +1,35 @@
 import React from 'react';
+import { reduxForm, Field } from 'redux-form';
 
 class Signup extends React.Component {
   render() {
     return (
-      <form>
-        <fieldset>
-          <label htmlFor="email">Email</label>
-        </fieldset>
-        <fieldset>
-          <label htmlFor="password">Password</label>
-        </fieldset>
-      </form>
+      <div>
+        <h2>Sign up</h2>
+        <form>
+          <fieldset>
+            <label htmlFor="email">Email</label>
+            <Field name="email" type="email" component="input" autoComplete="none" />
+          </fieldset>
+          <fieldset>
+            <label htmlFor="password">Password</label>
+            <Field
+              name="password"
+              type="password"
+              component="input"
+              autoComplete="none"
+            />
+          </fieldset>
+          <fieldset>
+            <label htmlFor="name">Name (Optional)</label>
+            <Field name="name" type="text" component="input" autoComplete="none" />
+          </fieldset>
+        </form>
+      </div>
     );
   }
 }
 
-export default Signup;
+export default reduxForm({
+  form: 'signup'
+})(Signup);
